822e347cbe2662a248680cfb31510629d7dd864d
akash patel <akash9753@gmail.com>
2023-10-16 09:45:09 -0700

    test eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function login(username: string): string {
-    return username;
+    const user = {
+        name: "rakesh",
+    };
+    const name = user.name;
+    return username + name;
 }
 
 login("akash");
