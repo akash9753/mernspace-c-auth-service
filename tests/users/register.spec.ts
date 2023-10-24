620545ee29e62695a260291c3cd647f8bed661f9
akash patel <akash9753@gmail.com>
2023-10-24 07:26:38 -0700

    DI

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,13 +1,28 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils";
+import { User } from "../../src/entity/User";
 
 describe("POST /auth/register", () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+    beforeEach(async () => {
+        await truncateTables(connection);
+    });
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe("Given all fields", () => {
         it("should return the 201 status code", async () => {
             //arrange
             const userData = {
                 firstName: "Rakesh",
-                lastname: "k",
+                lastName: "k",
                 email: "akash9753@gmail.com",
                 password: "secret",
             };
@@ -22,7 +37,7 @@ describe("POST /auth/register", () => {
             //arrange
             const userData = {
                 firstName: "Rakesh",
-                lastname: "k",
+                lastName: "k",
                 email: "akash9753@gmail.com",
                 password: "secret",
             };
@@ -34,6 +49,21 @@ describe("POST /auth/register", () => {
             expect(
                 (response.headers as Record<string, string>)["content-type"],
             ).toEqual(expect.stringContaining("json"));
+        });
+        it("should persist the user in the database", async () => {
+            //arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "k",
+                email: "akash9753@gmail.com",
+                password: "secret",
+            };
+            //Act
+            await request(app).post("/auth/register").send(userData);
+            //Assert
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(1);
         });
     });
     describe("Fields are missing", () => {});
