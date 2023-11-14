6fdeed4fea83b793e4761e306731935d99084048
akash9753 <akash9753@gmail.com>
2023-11-15 01:45:04 +0530

    timestamp

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 docker build -t auth-service:dev -f docker/development/Dockerfile .
 
 docker run --rm -it -v "%cd%":/usr/src/app -v /usr/src/app/node_modules --env-file "%cd%"/.env -p 5501:5501 -e NODE_ENV=development auth-service:dev
+
+https://gitfront.io/r/codersgyan/4QKc2vz4SuTU/mernspace-c-auth-service/
--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,15 +1,9 @@
-// import fs from "fs";
-// import path from "path"
 import { Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { JwtPayload } from "jsonwebtoken";
-// import createHttpError from "http-errors";
-// import { Config } from "../config";
-// import { AppDataSource } from "../config/data-source";
-// import { RefreshToken } from "../entity/RefreshToken";
 import { TokenService } from "../services/TokenService";
 import createHttpError from "http-errors";
 import { CredentialService } from "../services/CredentialService";
@@ -42,47 +36,14 @@ export class AuthController {
             });
             this.logger.info("User has been registerd", { id: user.id });
 
-            // let privateKey: Buffer;
-
-            // try {
-            //     privateKey = fs.readFileSync(path.join(__dirname,"../../certs/private.pem"))
-            // } catch(err){
-            //     const error = createHttpError(
-            //         500,
-            //         "Error while reading private key",
-            //     )
-            //     next(error)
-            //     return
-            // }
-
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
             };
 
-            //  const accessToken = sign(payload , privateKey, {
-            //     algorithm:"RS256",
-            //     expiresIn:"1h",
-            //     issuer:"auth-service",
-            //  })
 
             const accessToken = this.tokenService.generateAccessToken(payload);
 
-            //  const MS_IN_YEAR = 1000 * 60 * 60 * 365 // 1Y
-
-            //  const refreshTokenRepository = AppDataSource.getRepository(RefreshToken)
-
-            //  const newRefreshToken = await refreshTokenRepository.save({
-            //     user: user,
-            //     expiresAt: new Date(Date.now() + MS_IN_YEAR)
-            //  })
-
-            //  const refreshToken = sign(payload , Config.REFRESH_TOKEN_SECRET!, {
-            //     algorithm:"HS256",
-            //     expiresIn:"1y",
-            //     issuer:"auth-service",
-            //     jwtid: String(newRefreshToken.id)
-            //  })
 
             const newRefreshToken =
                 await this.tokenService.persistRefreshToken(user);
@@ -122,6 +83,11 @@ export class AuthController {
             return res.status(400).json({ errors: result.array() });
         }
         const { email, password } = req.body;
+        
+        this.logger.debug("New request to login a user", {
+            email,
+            password: "******",
+        });
 
         try {
             const user = await this.userService.findbyEmail(email);
