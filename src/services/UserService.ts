20ca8345464d8bf31b9b98cdf5efcd9e2b409b67
akash9753 <akash9753@gmail.com>
2024-02-25 06:40:16 +0530

    login service

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -64,4 +64,12 @@ export class UserService {
             throw error;
         }
     }
+
+    async findByEmail(email: string) {
+        return await this.userRepository.findOne({
+            where: {
+                email,
+            },
+        });
+    }
 }
