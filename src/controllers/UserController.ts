073bd9d14cacce63c3c7145b8b70bed40a6d49bf
akash9753 <akash9753@gmail.com>
2024-03-01 03:09:05 +0530

    update user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,9 +2,14 @@ import { NextFunction, Response } from "express";
 import { UserService } from "../services/UserService";
 import { CreateUserRequest } from "../types";
 import { Roles } from "../constants";
+import { Logger } from "winston";
+import createHttpError from "http-errors";
 
 export class UserController {
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private logger: Logger,
+    ) {}
 
     async create(req: CreateUserRequest, res: Response, next: NextFunction) {
         const { firstName, lastName, email, password } = req.body;
@@ -14,9 +19,38 @@ export class UserController {
                 lastName,
                 email,
                 password,
-                role: Roles.MANAGER,
+                role: Roles.CUSTOMER,
             });
             res.status(201).json({ id: user.id });
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    async update(req: CreateUserRequest, res: Response, next: NextFunction) {
+        // In our project: We are not allowing user to change the email id since it is used as username
+        // In our project: We are not allowing admin user to change others password
+
+        const { firstName, lastName, role } = req.body;
+        const userId = req.params.id;
+
+        if (isNaN(Number(userId))) {
+            next(createHttpError(400, "Invalid url param."));
+            return;
+        }
+
+        this.logger.debug("Request for updating a user", req.body);
+
+        try {
+            await this.userService.update(Number(userId), {
+                firstName,
+                lastName,
+                role,
+            });
+
+            this.logger.info("User has been updated", { id: userId });
+
+            res.json({ id: Number(userId) });
         } catch (err) {
             next(err);
         }
