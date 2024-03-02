19cb55d2ddd5ff3975cb0c366608271864d2c85f
akash9753 <akash9753@gmail.com>
2024-03-02 07:32:51 +0530

    role tenant id

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,20 @@
 import { Repository } from "typeorm";
 import bcrypt from "bcrypt";
 import { User } from "../entity/User";
-import { UserData, LimitedUserData } from "../types";
+import { LimitedUserData, UserData } from "../types";
 import createHttpError from "http-errors";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
-    async create({ firstName, lastName, email, password, role }: UserData) {
+    async create({
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        tenantId,
+    }: UserData) {
         const user = await this.userRepository.findOne({
             where: { email: email },
         });
@@ -18,7 +25,6 @@ export class UserService {
         // Hash the password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-
         try {
             return await this.userRepository.save({
                 firstName,
@@ -26,6 +32,7 @@ export class UserService {
                 email,
                 password: hashedPassword,
                 role,
+                tenant: tenantId ? { id: tenantId } : undefined,
             });
         } catch (err) {
             const error = createHttpError(
--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,4 +1,5 @@
-import { DataSource } from "typeorm";
+import { DataSource, Repository } from "typeorm";
+import { Tenant } from "../../src/entity/Tenant";
 
 export const truncateTables = async (connection: DataSource) => {
     const entities = connection.entityMetadatas;
@@ -12,7 +13,6 @@ export const isJwt = (token: string | null): boolean => {
     if (token === null) {
         return false;
     }
-
     const parts = token.split(".");
     if (parts.length !== 3) {
         return false;
@@ -23,7 +23,15 @@ export const isJwt = (token: string | null): boolean => {
             Buffer.from(part, "base64").toString("utf-8");
         });
         return true;
-    } catch (error) {
+    } catch (err) {
         return false;
     }
+};
+
+export const createTenant = async (repository: Repository<Tenant>) => {
+    const tenant = await repository.save({
+        name: "Test tenant",
+        address: "Test address",
+    });
+    return tenant;
 };
