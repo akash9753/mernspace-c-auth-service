8be4c170c8844f798aa703bbd69f6b081b7fef5b
akash9753 <akash9753@gmail.com>
2024-03-16 19:48:12 +0530

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,6 @@ module.exports = {
         "dot-notation": "error",
         "@typescript-eslint/no-misused-promises": "off",
         "@typescript-eslint/require-await": "off",
-        "@typescript-eslint/no-unsafe-assignment":"off"
+        "@typescript-eslint/no-unsafe-assignment": "off",
     },
 };
